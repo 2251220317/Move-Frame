af83c0165f79f48db5ee1bc5f2f927d252fec4dc
2251220317 <2251220317@qq.com>
2020-08-08 18:47:55 +0800

    兼容透明度 兼容css普通样式

--- move.js
+++ move.js
@@ -1,24 +1,36 @@
-//移动 1.1
+//移动 1.2
 function move(obj,name,target,fn){
 			clearInterval(obj.timer)
 			
 			obj.timer=setInterval(function () {
-				let nowStyle=parseInt(getComputedStyle(obj)[name])
-				let speed=(target-nowStyle)/10
-				
-				if(speed>0){
-					speed=Math.ceil(speed)
+				let nowStyle
+				if(name=='opacity'){
+					nowStyle=getComputedStyle(obj)[name]*100
+				}
+				else if(name=='width'||name=='height'||name=='font-size'||name=='border-width'||name=='border-radius'){
+					nowStyle=parseInt(getComputedStyle(obj)[name])
 				}
 				else{
-					speed=Math.floor(speed)
+					nowStyle=getComputedStyle(obj)[name]
 				}
+				
 				
 				if(nowStyle==target){
 					clearInterval(obj.timer)
 					if(fn){fn()}
 				}
 				else{
-					obj.style[name]=nowStyle+speed+'px'
+					let speed=(target-nowStyle)/10
+					speed=(speed>0)?Math.ceil(speed):Math.floor(speed)
+					if(name=='opacity'){
+						obj.style[name]=(nowStyle+speed)/100
+					}
+					else if(name=='width'||name=='height'||name=='font-size'||name=='border-width'||name=='border-radius'){
+						obj.style[name]=nowStyle+speed+'px'
+					}
+					else{
+						obj.style[name]=target
+					}
 				}
 			},30)
 		}
