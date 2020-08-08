81dcbb02cb68e7690e16e53a17abb45559a3fe08
2251220317 <2251220317@qq.com>
2020-08-08 17:48:59 +0800

    move 1.2 兼容链式运动 兼容字体大小变化

--- move.js
+++ move.js
@@ -1,15 +1,23 @@
-//移动 1.0
-function move(obj,name,target){
+//移动 1.1
+function move(obj,name,target,fn){
 			clearInterval(obj.timer)
 			
 			obj.timer=setInterval(function () {
 				let nowStyle=parseInt(getComputedStyle(obj)[name])
+				let speed=(target-nowStyle)/10
+				
+				if(speed>0){
+					speed=Math.ceil(speed)
+				}
+				else{
+					speed=Math.floor(speed)
+				}
 				
 				if(nowStyle==target){
 					clearInterval(obj.timer)
+					if(fn){fn()}
 				}
 				else{
-					let speed=(target-nowStyle)/10
 					obj.style[name]=nowStyle+speed+'px'
 				}
 			},30)
